120226bf169e1f87cc0262e1866021c56efcd15a
Conrad Kreyling <conrad@kreyling.biz>
2021-08-11 21:11:26 -0400

    Use LovyanGFX, DMA

--- src/ViewThread.hpp
+++ src/ViewThread.hpp
@@ -6,7 +6,8 @@
 #include <Ticker.h>
 #include <lvgl.h>
 #include <SparkFunBME280.h>
-#include <TFT_eSPI.h>
+#include <LovyanGFX.hpp>
+#include <LGFX_AUTODETECT.hpp> 
 #include "status.h"
 #include "BucketThread.hpp"
 #include "Model.hpp"
@@ -30,27 +31,22 @@ public:
     if(!Subscriber<WifiData>::init()) return false;
     setInterval(5);
     
-    // Init the back-light LED PWM
-    ledcSetup(BLK_PWM_CHANNEL, 44100, 8);
-    ledcAttachPin(TFT_BL, BLK_PWM_CHANNEL);
-    brightness(10);
-
     // LVGL INITIALIZATION ROUTINES HERE!
     lv_init();
 
-    tft.init(); /* TFT init */
-    tft.setRotation(TFT_ROTATION); /* Landscape orientation */
-    tft.invertDisplay(TFT_INVERTED);
-    //Log.noticeln("TFT initialized.");
+    lcd.init(); 
+    lcd.initDMA();
+    lcd.setRotation(1);
+    lcd.startWrite(); // hold the line.
     
-    lv_disp_buf_init(&disp_buf, buf, NULL, LV_HOR_RES_MAX * 10);
+    lv_disp_buf_init(&disp_buf, buf1, buf2, LV_HOR_RES_MAX * 10);
 
     lv_disp_drv_t disp_drv;
     lv_disp_drv_init(&disp_drv);
-    disp_drv.hor_res = TFT_HEIGHT;
-    disp_drv.ver_res = TFT_WIDTH; //rotation
+    disp_drv.hor_res = LV_HOR_RES_MAX;
+    disp_drv.ver_res = LV_VER_RES_MAX; //rotation
     disp_drv.flush_cb = my_disp_flush;
-    disp_drv.user_data = (void*)(&tft);
+    disp_drv.user_data = (void*)(&lcd);
     disp_drv.buffer = &disp_buf;
     lv_disp_drv_register(&disp_drv);
     m_Ticker.attach_ms(lv_tick_interval_ms, lv_tick_handler);
@@ -72,13 +68,15 @@ public:
 
 protected:
   lv_disp_buf_t disp_buf;
-  lv_color_t buf[LV_HOR_RES_MAX * 10];
+  lv_color_t buf1[LV_HOR_RES_MAX * 10];
+  lv_color_t buf2[LV_HOR_RES_MAX * 10];
   Ticker m_Ticker;
-  TFT_eSPI tft;
+  LGFX lcd;
   int m_Brightness;
 
   void checkSubs() {
     PowerData powerData;
+    WifiData wifiData;
     BME280_SensorMeasurements measurements;
     if(Subscriber<BME280_SensorMeasurements>::get(&measurements)) {
       String tempString = String((int)(measurements.temperature * 1.8f + 32)) + String("°F");
@@ -108,7 +106,6 @@ protected:
       }
     }
 
-    /*
     if(Subscriber<WifiData>::get(&wifiData)) {
       if(wifiData.connected()) {
         if(wifiData.rssi >= -60) {
@@ -126,7 +123,6 @@ protected:
         lv_img_set_src(WifiIcon, &wifi_disconnected);
       }
     }
-    */
   }
 
 private:
@@ -134,13 +130,10 @@ private:
   static void lv_tick_handler(void) { lv_tick_inc(lv_tick_interval_ms); }
   static void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p)
   {
-    TFT_eSPI* tft = (TFT_eSPI*)disp->user_data;
+    LGFX* lcd = (LGFX*)disp->user_data;
     uint32_t w = (area->x2 - area->x1 + 1);
     uint32_t h = (area->y2 - area->y1 + 1);
-    tft->startWrite();
-    tft->setAddrWindow(area->x1, area->y1, w, h);
-    tft->pushColors( ( uint16_t * )&color_p->full, w * h, true );
-    tft->endWrite();
+    lcd->pushImageDMA(area->x1, area->y1, w, h, ( uint16_t * )&color_p->full);
     lv_disp_flush_ready(disp);
   }
 };
